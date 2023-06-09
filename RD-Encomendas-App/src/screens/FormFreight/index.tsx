fc42eb03ad7ab5a16d58ac040278b9b62c7b3247
Wesley Ferreira <wesleyfc00@hotmail.com>
2023-06-09 11:26:12 -0300

    chore: initial select in screen freight

--- RD-Encomendas-App/src/screens/FormFreight/index.tsx
+++ RD-Encomendas-App/src/screens/FormFreight/index.tsx
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import { styles } from './styles';
 import { CityProps } from '../../@types/cities';
 import { RouteProp } from '@react-navigation/native';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import { Adapt, Select, SelectProps, Sheet } from 'tamagui';
+import { citiesMinasGerais } from '../../data/cities';
+import { Check } from 'phosphor-react-native';
 
 type RoutParamsProps = {
     city: CityProps
+}
+
+const SelectCity = (props: SelectProps) => {
+    const [selectedCity, setSelectedCity] = useState<string>("");
+
+    return (
+        <Select id="currentCity" value={selectedCity} onValueChange={setSelectedCity}>
+            <Select.Trigger width={180} >
+                <Select.Value placeholder="Something" />
+            </Select.Trigger>
+
+            <Adapt when="sm" platform="touch">
+                <Sheet native modal dismissOnSnapToBottom>
+                    <Sheet.Frame>
+                        <Sheet.ScrollView>
+                            <Adapt.Contents />
+                        </Sheet.ScrollView>
+                    </Sheet.Frame>
+                    <Sheet.Overlay />
+                </Sheet>
+            </Adapt>
+
+            <Select.Content zIndex={99}>
+                <Select.Viewport>
+                    {citiesMinasGerais.map((city, i) => (
+                        <Select.Item index={i} key={i} value={city.name}>
+                            <Select.ItemText>
+                                {city.name}
+                            </Select.ItemText>
+                            <Select.ItemIndicator marginLeft="auto">
+                                <Check size={16} />
+                            </Select.ItemIndicator>
+                        </Select.Item>
+                    ))}
+                </Select.Viewport>
+            </Select.Content>
+        </Select >
+    )
 }
 
 export function FormFreight() {
@@ -18,6 +59,7 @@ export function FormFreight() {
             <Text style={styles.pageTitle}>{city.name}</Text>
             <Text style={styles.pageInfo}>Tarifa : {city.serviceCharge}</Text>
             <Text style={styles.pageInfo}>{city.servicesIncluded}</Text>
+            <SelectCity native />
         </View>
     );
 }
